f9c8d6a620bb086dfd281fe20e25c7402870277b
MariaPerezRuizL <perezeruizmaria@gmail.com>
2017-05-01 18:07:01 +0200

    Commit

--- app/src/main/java/com/example/maria/proyectofinal_v3/CategoriaUsuario.java
+++ app/src/main/java/com/example/maria/proyectofinal_v3/CategoriaUsuario.java
@@ -185,7 +185,6 @@ public class CategoriaUsuario extends AppCompatActivity {
                 CategoriaUsuario.this.startActivity(intent);
 
             }
-
           // servicios=datosServicios;
 
         }
